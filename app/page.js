7c28b78fc0ab8338b7662e962435f792d56dd2b2
ananyagupta222201-sys <ananyagupta222201@gmail.com>
2026-08-08 16:28:16 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', padding: '50px', fontFamily: 'Arial'}}>
+      <h1>Mera Kharcha App 💰</h1>
+      <p>App Live ho gaya! Ab kharcha track karo</p>
+    </div>
+  )
+}
